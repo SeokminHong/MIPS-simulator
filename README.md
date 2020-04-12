dc3fc76c1304139b433989a92a445b5b93e2c86a
seokmin <kyma123@unist.ac.kr>
2020-04-12 21:32:13 +0900

    add examples and function for parse inputs

--- README.md
+++ README.md
@@ -7,8 +7,8 @@
 
 ## Run
 
-    $ ./mips-sim < input
+    $ ./mips-sim {Program_binary} {Number_of_cycles} [Initial_register_state]
 
-OR
+Example:
 
-    $ ./mips-sim < input > output.txt
+    $ ./mips-sim examples/p1/p1e1.txt 1 init.txt
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <string>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello!" << std::endl;
+    std::ifstream source;
+    source.open(argv[1]);
+
+    std::string line;
+    
+    while (source >> line, !source.fail())
+    {
+        int num = std::stoi(line, 0, 16);
+        printf("%s\tDec: %d\tHex: %x\n", line.c_str(), num, num);
+    }
 }
