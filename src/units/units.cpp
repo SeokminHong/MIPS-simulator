149d71ad27cedb2adf9fd1a40b5c86644b2c6877
Seokmin Hong <ghdtjrals240@naver.com>
2020-05-09 13:01:32 +0000

    주석 제거

--- src/units/units.cpp
+++ src/units/units.cpp
@@ -121,67 +121,6 @@ std::tuple<ctrl_EX, ctrl_M, ctrl_WB> Control(const inst_t& inst)
             }
         }
     }
-    
-    
-    /*switch (inst.base.op)
-    {
-        // R-format
-        case 0:
-        {
-            ex.regDst = 1;
-            ex.aluOP1 = 1;
-            ex.aluOP0 = 0;
-            ex.aluSrc = 0;
-            m.branch = 0;
-            m.memRead = 0;
-            m.memWrite = 0;
-            wb.regWrite = 1;
-            wb.memtoReg = 0;
-            break;
-        }
-        // beq
-        case 4:
-        {
-            ex.regDst = 0;
-            ex.aluOP1 = 0;
-            ex.aluOP0 = 1;
-            ex.aluSrc = 0;
-            m.branch = 1;
-            m.memRead = 0;
-            m.memWrite = 0;
-            wb.regWrite = 0;
-            wb.memtoReg = 0;
-            break;
-        }
-        // lw
-        case 35:
-        {
-            ex.regDst = 0;
-            ex.aluOP1 = 0;
-            ex.aluOP0 = 0;
-            ex.aluSrc = 1;
-            m.branch = 0;
-            m.memRead = 1;
-            m.memWrite = 0;
-            wb.regWrite = 1;
-            wb.memtoReg = 1;
-            break;
-        }
-        // sw
-        case 43:
-        {
-            ex.regDst = 0;
-            ex.aluOP1 = 0;
-            ex.aluOP0 = 0;
-            ex.aluSrc = 1;
-            m.branch = 0;
-            m.memRead = 0;
-            m.memWrite = 1;
-            wb.regWrite = 0;
-            wb.memtoReg = 0;
-            break;
-        }
-    }*/
     return std::make_tuple(std::move(ex), std::move(m), std::move(wb));
 }
 
